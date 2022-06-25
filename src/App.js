0e4ec4f5ddf338243a4ddc0abf4d96d7fc031b24
Łukasz Stefański <lukasz.stefanski@rst.com.pl>
2022-06-25 19:10:16 +0200

    Remove persistStore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { EventrixProvider, PersistStoreGate } from 'eventrix';
+import { EventrixProvider } from 'eventrix';
 import eventrix from './eventrix/index';
 import Logo from './components/Logo';
-import Loader from './components/Loader';
 import Chairs from './components/Chairs';
 import Desks from './components/Desks';
 import Wardrobes from './components/Wardrobes';
@@ -9,16 +8,14 @@ import './App.css';
 
 const App = () => (
     <EventrixProvider eventrix={eventrix}>
-        <PersistStoreGate eventrix={eventrix} loader={Loader}>
-            <div className='app'>
-                <Logo />
-                <div className='items'>
-                    <Chairs />
-                    <Desks />
-                    <Wardrobes />
-                </div>
+        <div className='app'>
+            <Logo />
+            <div className='items'>
+                <Chairs />
+                <Desks />
+                <Wardrobes />
             </div>
-        </PersistStoreGate>
+        </div>
     </EventrixProvider>
 )
 
--- src/eventrix/index.js
+++ src/eventrix/index.js
@@ -1,4 +1,4 @@
-import { Eventrix, connectPersistStore, EventrixDebugger } from 'eventrix';
+import { Eventrix, EventrixDebugger } from 'eventrix';
 import chairsEventsReceiver from './receivers/chairs';
 import desksEventsReceiver from './receivers/desks';
 import wardrobesEventsReceiver from './receivers/wardrobes';
@@ -9,17 +9,9 @@ const initialState = {
     wardrobes: 0,
 };
 
-const config = {
-    blackList: ['chairs'],
-    storage: localStorage,
-    storageKey: 'exampleData',
-};
-
 const eventsReceivers = [chairsEventsReceiver, desksEventsReceiver, wardrobesEventsReceiver];
 
 const eventrix = new Eventrix(initialState, eventsReceivers);
-
-connectPersistStore(eventrix, config);
 
 const eDebugger = new EventrixDebugger(eventrix);
 eDebugger.start();
